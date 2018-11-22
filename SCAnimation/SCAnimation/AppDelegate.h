d87301b9987d3847624aa0dde5a574826f4341b1
Lynn Liu <linjieliu7@gmail.com>
2018-11-22 10:19:48 +0800

    first commit

---
+++ SCAnimation/SCAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCAnimation
+//
+//  Created by admin on 2018/11/22.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SCAnimation/SCAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCAnimation
+//
+//  Created by admin on 2018/11/22.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
